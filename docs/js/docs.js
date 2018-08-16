2963eecf9253c42be42bf8f1e9e5e8e3f8e759ac
Gao Wang <gaow@uchicago.edu>
2018-08-16 16:32:02 -0500

    Update documentation

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,6 +1,6 @@
 var DSCArray = []
 var DSCDict = {}
 var DSCArrayMap = {}
-var numerical_resultsArray = ['index']
-var numerical_resultsDict = {'Numerical-Comparisons-1': 'numerical_results'}
-var numerical_resultsArrayMap = {'numerical_results': 'Numerical Comparisons'}
+var numerical_resultsArray = ['Untitled', 'index']
+var numerical_resultsDict = {'d1-readRDS(-susie-comparison-Power-comparison-0722-cluster-prob-estvar-true.rds-)-1': 'Untitled', 'Numerical-Comparisons-1': 'numerical_results'}
+var numerical_resultsArrayMap = {'Untitled': "d1 = readRDS('susie_com ... r_true.rds')", 'numerical_results': 'Numerical Comparisons'}

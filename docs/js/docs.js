f01a8ef408c241903308dd1520d571ab0f15ab93
Gao Wang <gaow@uchicago.edu>
2018-09-11 09:16:08 -0500

    Update documentation

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -2,5 +2,5 @@ var finemapping_benchmarkArray = []
 var finemapping_benchmarkDict = {'Finemapping-benchmark-1': 'finemapping'}
 var finemapping_benchmarkArrayMap = {'finemapping': 'Finemapping benchmark'}
 var manuscript_resultsArray = ['pedagogical_example', 'numerical_results', 'motivating_example', 'Li_2016_splice_QTL']
-var manuscript_resultsDict = {'Splicing-QTL-analysis:-application-to-Li-et-al-2016-1': 'Li_2016_splice_QTL', 'A-demonstration-of-SuSiE-s-motivations-1': 'motivating_example', 'Numerical-Comparisons-1': 'numerical_results', 'A-pedagogical-example-of-fine-mapping-problem-1': 'pedagogical_example'}
-var manuscript_resultsArrayMap = {'Li_2016_splice_QTL': 'Splicing QTL analysis:  ... i et al 2016', 'motivating_example': 'A demonstration of SuSi ... motivations', 'numerical_results': 'Numerical Comparisons', 'pedagogical_example': 'A pedagogical example o ... ping problem'}
+var manuscript_resultsDict = {'Splicing-QTL-analysis:-application-to-Li-et-al-2016-1': 'Li_2016_splice_QTL', 'A-demonstration-of-SuSiE-s-motivations-1': 'motivating_example', 'Numerical-Comparisons-1': 'numerical_results', 'A-hard-case-fine-mapping-example-1': 'pedagogical_example'}
+var manuscript_resultsArrayMap = {'Li_2016_splice_QTL': 'Splicing QTL analysis:  ... i et al 2016', 'motivating_example': 'A demonstration of SuSi ... motivations', 'numerical_results': 'Numerical Comparisons', 'pedagogical_example': 'A hard case fine-mapping example'}

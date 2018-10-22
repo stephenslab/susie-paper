590c241c1cad0601d288801a30fa30d7a9f49267
Gao Wang <gaow@uchicago.edu>
2018-10-22 14:23:39 -0500

    Update documentation

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,6 +1,6 @@
 var finemapping_benchmarkArray = []
 var finemapping_benchmarkDict = {'Finemapping-benchmark-1': 'finemapping'}
 var finemapping_benchmarkArrayMap = {'finemapping': 'Finemapping benchmark'}
-var manuscript_resultsArray = ['pedagogical_example', 'numerical_results', 'motivating_example', 'Li_2016_splice_QTL']
-var manuscript_resultsDict = {'Splicing-QTL-analysis:-application-to-Li-et-al-2016-1': 'Li_2016_splice_QTL', 'A-demonstration-of-SuSiE-s-motivations-1': 'motivating_example', 'Numerical-Comparisons-1': 'numerical_results', 'A-pedagogical-example-of-fine-mapping-problem-1': 'pedagogical_example'}
-var manuscript_resultsArrayMap = {'Li_2016_splice_QTL': 'Splicing QTL analysis:  ... i et al 2016', 'motivating_example': 'A demonstration of SuSi ... motivations', 'numerical_results': 'Numerical Comparisons', 'pedagogical_example': 'A pedagogical example o ... ping problem'}
+var manuscript_resultsArray = ['pedagogical_example', 'numerical_results', 'motivating_example', 'Untitled', 'Li_2016_splice_QTL']
+var manuscript_resultsDict = {'Splicing-QTL-analysis:-application-to-Li-et-al-2016-1': 'Li_2016_splice_QTL', 'dat-readRDS(-home-gaow-GIT-github-mvarbvs-dsc-susie-comparison-ROC-1008-prior-0p1-null-0p0-two.rds-)-1': 'Untitled', 'A-demonstration-of-SuSiE-s-motivations-1': 'motivating_example', 'Numerical-Comparisons-1': 'numerical_results', 'A-pedagogical-example-of-fine-mapping-problem-1': 'pedagogical_example'}
+var manuscript_resultsArrayMap = {'Li_2016_splice_QTL': 'Splicing QTL analysis:  ... i et al 2016', 'Untitled': "dat = readRDS('/home/ga ... p0_two.rds')", 'motivating_example': 'A demonstration of SuSi ... motivations', 'numerical_results': 'Numerical Comparisons', 'pedagogical_example': 'A pedagogical example o ... ping problem'}
